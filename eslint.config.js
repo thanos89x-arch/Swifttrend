3e70bcdd46ca79f1a7d343623e740c4bf397fc43
thanos89x <thanos89x@gmail.com>
2026-04-18 11:44:29 +0200

    fix(lint): exclude legacy monolith from ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'src/SwiftTrendAI_v22.jsx', 'src/App.jsx']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
